a1f7f011b37c5a54a8d612213d6ccfb7e6669ed4
GustavoFabres <guga.fabres@gmail.com>
2025-09-16 19:49:56 -0300

    Add files via upload

---
+++ 01-css-globa/main.jsx
@@ -0,0 +1,14 @@
+import './styles/index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {NavBar1}  from "./component/navBar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from './App.jsx'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
